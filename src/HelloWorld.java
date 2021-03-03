b8b1c1455819345a3a80dd92fe9ec382d4003541
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-03-03 13:49:07 -0600

    syntax, types, variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,52 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
-        System.out.print("Hello, World!");
+//        System.out.print("Hello, World!");
+
+//        int myFavoriteNumber = 777;
+//        System.out.print(myFavoriteNumber);
+
+//        String myString;
+//        myString = "sup, i'm a string";
+//        System.out.print(myString);
+////        i notice that the char doesn't work for this. I got prompted to use string instead of var.
+
+//        double myString;
+//        myString = 3.14159;
+//        System.out.print(myString);
+        //char does not work, var does. prompted by intelli j to use double.
+
+//        float myNumber = (float) 3.14;
+//        System.out.print(myNumber);
+        // it says illegal for empty, doesn't like 3.14 as long. 123L works for long. 123 worked because..
+        // it didn't have a decimal??
+        // for float, i got prompted to cast float
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+        // i got 5 and 6 on seperate lines
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+        // i got 6 and 6 for this one
+        // i understand that the pre increment, and post increment lead to different behaviors of what to do with the int variable.
+        // i think because the post increment will increment after the code is executed, which added the one to the number 5
+
+        //class doesn't like being a variable.. it seems...
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        System.out.println(three);
+        // got an error. probly cuz "three" is a string that cannot be converted into an int
+        // didn't like this either with a different error. incompatible types. same reasons, different nests.
+
+
+
+
     }
 
 }
---
+++ src/SyntaxTypesVariablesLec.java
@@ -0,0 +1,26 @@
+public class SyntaxTypesVariablesLec {
+
+    public static void main(String[] args) {
+
+        {
+
+//            byte bite1 = 111;
+//            byte bite2 = 1;
+//            var output =bite2 + bite1;
+//            System.out.println(((Object)output)
+//            .getClass()
+//            .getName());
+
+//            short shrt = 32767;
+
+            int minValue = Integer.MIN_VALUE;
+            System.out.println(minValue -1);
+
+            int maxValue = Integer.MAX_VALUE;
+            System.out.println(maxValue + (maxValue +3));
+
+        }
+
+        }
+    }
+
