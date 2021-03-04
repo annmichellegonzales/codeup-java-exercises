0fe437d42c38cfbc954c629733d065c886b187bc
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-03-04 14:45:40 -0600

    Problem #1

--- src/ConsoleIOLecture.java
+++ src/ConsoleIOLecture.java
@@ -106,22 +106,106 @@ public class ConsoleIOLecture {
 //            System.out.println(words);
 
 //        }
+//
+//                /* TODO: use the scanner to take in your birthday as an integer and then name of the month you were born and print it back out
+//            - remember to first prompt the user for the birthday day of the month and store as an integer
+//            - next take in name of the month with .nextLine()
+//            - print out the result
+//            - use a fix to prevent the month name input from being skipped
+//         */
+//
+//                Scanner sc = new Scanner(System.in);
+//                System.out.println("What date were you born?");
+//                Byte date = sc.nextByte();
+//                sc.nextLine();
+//                System.out.println("What month were you born?");
+//                String month = sc.nextLine();
+//                System.out.println("Your birthday is on " + month + " " + date);
 
-                /* TODO: use the scanner to take in your birthday as an integer and then name of the month you were born and print it back out
-            - remember to first prompt the user for the birthday day of the month and store as an integer
-            - next take in name of the month with .nextLine()
-            - print out the result
-            - use a fix to prevent the month name input from being skipped
-         */
 
-                Scanner sc = new Scanner(System.in);
-                System.out.println("What date were you born?");
-                Byte date = sc.nextByte();
-                sc.nextLine();
-                System.out.println("What month were you born?");
-                String month = sc.nextLine();
-                System.out.println("Your birthday is on " + month + " " + date);
+//                public class ControlStatementsLecture {
+//
+//                        public static void main(String[] args) {
 
+
+                                // ==================== Slide 3
+
+                                // no >== ,or <==, or === because only value comparisons are available
+                                // truthy and falsy values don't exist in Java
+
+
+                                // ==================== Slide 5
+
+                                // && will only check second value if first is true
+                                // & will check both
+                                // this can be used to check what would otherwise result in a syntax error
+                                // same comparison process for || and |
+
+                                // x will only increment in second condition if first condition is true using &&
+//                int x = 2;
+//                int y = 3;
+//
+//                if (false && ++x == y) {
+//                    System.out.println("Equal");
+//                } else {
+//                    System.out.println("Not equal");;
+//                }
+//                System.out.println(x);
+
+
+                                // ==================== Slide 7
+
+                                // DO NOT COMPARE STRINGS WITH == !!!!
+
+//                Scanner sc = new Scanner(System.in);
+//                String cat1 = sc.nextLine();
+//                String cat2 = "cat";
+//                System.out.println(cat1);
+//                System.out.println(cat2);
+//                System.out.println(cat1 == cat2);
+
+                                // do use equals(),
+
+//    boolean stringValuesAreEqual = "Test".equals("Tes");
+//    System.out.printf("The result is: %s", stringValuesAreEqual);
+
+
+                                // or use equalsIgnoreCase()
+
+//String hello = "hello";
+//System.out.println(hello.equalsIgnoreCase("HeLlO"));
+
+                                // does not equal
+
+//String tech = "tech";
+//System.out.println(!tech.equalsIgnoreCase("tool"));
+
+
+
+                                // ==================== Slides 8 - 16
+
+                                // basically the same as JS :)
+
+
+//            String caseSwitch = "Fred";
+//            switch (caseSwitch) {
+//                case "bob":
+//                    System.out.println("Case 1");
+//                    break;
+//                case "fred":
+//                    System.out.println("Case 2");
+//                    break;
+//                default :
+//                    System.out.println("Default case");
+//                    break;
+//            }
+
+
+
+
+//                        }
+
+//                }
 
 
     }
---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,11 @@
+public class ControlFlowExercises {
+
+    public static void main(String[] args) {
+        Integer i = 5;
+        while(i<= 15) {
+            System.out.println("i is " + i);
+            i++;
+        }
+    }
+
+}
