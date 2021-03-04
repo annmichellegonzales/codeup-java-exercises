31fc67d9fb43374e019a30deb0d0fc7e0cb63e33
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-03-04 15:16:46 -0600

    count by 2's

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,13 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-        Integer five = 5;
-        while (five <= 15) {
-            System.out.print(five++ + " ");
-        }
+        int zero = 0;
+        do {
+
+            System.out.println(zero);
+            zero += 2;
+
+        } while (zero <= 100);
+
     }
 }
