7216a686d23314395917fa797ed6a4ec0c6a5ddc
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-03-04 16:01:12 -0600

    Fizz Buzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,13 +1,20 @@
+import java.util.stream.IntStream;
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-        int zero = 0;
-        do {
 
-            System.out.println(zero);
-            zero += 2;
+        int input = 0;
 
-        } while (zero <= 100);
+        if (input % 15 == 0) {
+            System.out.println("FizzBuzz");
+        }
+        if (input % 3 == 0) {
+            System.out.println("Fizz");
+        }
+        if (input % 5 == 0) {
+            System.out.println("Buzz");
+        }
+        System.out.println(String.valueOf(input));
 
     }
 }
