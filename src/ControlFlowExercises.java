7e6bae22943de9e20c40a4cfdf676758fd2238ba
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-03-04 16:54:34 -0600

    asking user to proceed to table

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -22,16 +22,22 @@ public class ControlFlowExercises {
         System.out.println("What number would you like to go up to?");
         int yourInt = sc.nextInt();
 
+        System.out.println("Would you like a table?");
+        String response = sc.next();
 
-        System.out.print("number\t \t |" + "squared\t \t \t |" + "cubed\n -----------------------------------------\n");
+        if(response.equals("yes")) {
 
-        for ( int count = 1 ; count <= yourInt ; count++)
-        {
-            int squared = count * count;
-            int cubed = count * count * count;
-            System.out.println(count + "\t \t \t | " + squared + "\t \t \t \t| " + cubed);
+
+            System.out.print("number\t \t|\t" + "squared\t \t \t|\t" + "cubed\n-----------------------------------------\n");
+
+            for (int count = 1; count <= yourInt; count++) {
+                int squared = count * count;
+                int cubed = count * count * count;
+                System.out.println(count + "\t \t \t|\t " + squared + "\t \t \t \t|\t" + cubed);
+            }
+        } else {
+            System.out.println("Maybe next time, red!");
         }
-
 
 
     }
