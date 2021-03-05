009884cac01b9fcb9bbfde77a89a3c2f70c0e1b7
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-03-05 12:32:13 -0600

    strings

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,6 +2,22 @@ public class StringExercise {
 
     public static void main(String[] args) {
 
+        String pink = "We don't need no education";
+        String floyd = "We don't need no thought control";
+        System.out.println(pink);
+        System.out.println(floyd);
+
+        String checkThis = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(checkThis);
+
+        String inWindows = "In windows, the main drive is usually C:\\";
+        System.out.println(inWindows);
+
+        //In windows, the main drive is usually C:\
+
+//        I can do backslashes \, double backslashes \\,
+//        and the amazing triple backslash \\\!
+
     }
 
 }
