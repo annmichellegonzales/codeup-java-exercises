ef71fcf8b3e98aec35a8df8e67aab108fa9d3e54
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-03-12 12:34:41 -0600

    array updated exercises

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -7,16 +7,37 @@ public class ServerNameGenerator {
         String[] adjectives = {"morose", "shadowy", "dark", "serene", "placid",
                 "velvety", "black", "macabre", "ornate", "witchy"};
 
-        String[] nouns = {"mortal", "ghost", "attic", "raven", "grave",
+        String[] nouns = {"fortress", "ghost", "attic", "raven", "grave",
                 "shroud", "midnight", "despair", "spectre", "castle"};
 
-        int idxAdj = new Random().nextInt(nouns.length);
+
+        int idxAdj = new Random().nextInt(adjectives.length);
         String randomAdj = (adjectives[idxAdj]);
 
         int idxNoun = new Random().nextInt(nouns.length);
         String randomNoun = (nouns[idxNoun]);
 
-        System.out.println("Here is your server name: " + randomAdj + "-" + randomNoun);
+        System.out.println("Welcome, mortal, to the ^v^ " + randomAdj + "-" + randomNoun + " ^v^ server.");
 
     }
 }
+
+
+
+////SERVERNAME GENERATOR
+//public class ServerNameGenerator {
+//    public static String[] adjectives = {"funny", "lackadaisical", "prompt", "fastidious", "prickly", "amazing", "incredible", "outdated", "pristine", "comical"};
+//    public static String[] nouns = {"desk", "guitar", "house", "volkswagen", "lawnmower", "duck", "chair", "poster", "event", "candle"};
+//    public static void main(String[] args) {
+//        System.out.println("Here is your server name:");
+//        System.out.println(getWord(adjectives) + "-" + getWord(nouns));
+//    }
+//    public static String getWord(String[] choices) {
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(choices.length);
+//        return choices[randomNumber];
+//    }
+//}
+
+
+
---
+++ src/movies/Movie.java
@@ -0,0 +1,31 @@
+package movies;
+
+//class Movie {
+//
+//    private String name;
+//    private String category;
+
+//    public Movie() { this.category = "horror"; }
+
+//    public String shareMovie() { return category; }
+
+
+
+//    public Movie(String name, String category) {
+//        this.name = name;
+//        this.category = category;
+//
+//    }
+
+//    public String shareMovie(String title) {
+//        String myMovie = this.name("Titanic");
+//        System.out.println();
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        shareMovie();
+//
+//    }
+//
+//}
