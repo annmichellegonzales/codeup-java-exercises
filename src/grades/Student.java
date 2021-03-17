6421069c36970a32381a99aaed406d804aabd93b
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-03-17 18:55:10 -0500

    student grades app, incomplete

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,34 +1,53 @@
 package grades;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Student {
 
-    public static void main(String[] args) {
+    private static ArrayList<Integer> grades;
+    private String name;
+
+
+    public Student(String name, ArrayList<Integer> grades) {
+        this.name = name;
+        this.grades=grades;
+    }
+
+
+
+    // returns the student's name
+    public static String getName(String name) {
+        return name;
+    }
+
+    public static void addGrade(int grade) {
+//        return getGradeAverage(grades.add(grade));
+    }
+
+    // returns the average of the students grades
+    public static double getGradeAverage(double average) {
+//        for (grades grade : )
 
     }
-//
-//    private String name;
-//    private List<Integer> grades();
-//
-//    public Student(String name, ArrayList<Integer> grades) {
-//        this.name = name;
-//        this.grades = grades;
-//    }
-//
-//    // returns the student's name
-//    public String getName(){
-//        return name;
-//    }
-//    // adds the given grade to the grades property
-//    public void addGrade(int grade){
-//        grades.add(grade);
-//    }
-//    // returns the average of the students grades
-//    public double getGradeAverage(){
-//        return 1;
-//    }
+
+    public static void main(String[] args) {
+
+        Student a = new Student("Ann", grades);
+        ArrayList<Integer> grades = new ArrayList<>();
+        grades.add(88);
+        grades.add(100);
+        grades.add(70);
+
+        System.out.println(a.name);
+        System.out.println(grades);
+        System.out.println(a.grades);
+        getName("Ann");
+        System.out.println(getName("Ann"));
+
+
+
+
+    }
 
 
 }
